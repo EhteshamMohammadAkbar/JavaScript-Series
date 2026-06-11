c211fbc8b985832044a89f6bae3103dcf59053e7
Ehtesham Akbar <youremail@example.com>
2026-06-11 23:35:14 +0100

    datatype.js

---
+++ datatype.js
@@ -0,0 +1,34 @@
+let someName = "Ehtesham";
+let someNameTypeOf = Number(someName);
+console.log(someNameTypeOf);
+console.log(typeof someNameTypeOf);
+
+let someNumber = 50;
+let someNumberTypeOf = String(someNumber);
+console.log(someNumberTypeOf);
+console.log(typeof someNumberTypeOf);
+
+let logIn = 1;
+let logInTypeOf = Boolean(logIn);
+console.log(logInTypeOf);
+console.log(typeof logInTypeOf);
+
+let signOut = 0;
+let signOutTypeOf = Boolean(signOut);
+console.log(signOutTypeOf);
+console.log(typeof signOutTypeOf);
+
+
+let signIn = "";
+let signInTypeOf = Boolean(signIn);
+console.log(signInTypeOf);
+console.log(typeof signInTypeOf);
+
+let logOut = "Ture/False??";
+let logOutTypeOf = Boolean(logOut);
+console.log(logOutTypeOf);
+console.log(typeof logOutTypeOf);
+
+
+
+
