cc4e2a72e19f4b34c880c1d3b42c43a6d0fedce6
Ehtesham Akbar <youremail@example.com>
2026-06-11 22:45:37 +0100

    first commit

---
+++ variable.js
@@ -0,0 +1,7 @@
+const otherId = 4004;
+let otherName = "Ehtesham";
+var otherEmail = "Hello@gmail.com";
+const otherPassword = "Hello5005";
+let otherAdress = "BaldiaTown";
+
+console.table([otherId, otherName, otherEmail, otherPassword, otherAdress]);
