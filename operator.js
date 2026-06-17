a8d28db410fac8c92054a4234a00935050492c3c
Ehtesham Akbar <youremail@example.com>
2026-06-17 22:54:31 +0100

    Operators.js

---
+++ operator.js
@@ -0,0 +1,51 @@
+
+let value = 3;
+let negvalue = -value;
+console.log(negvalue); // -3 (negative value ban gayi)
+
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 ** 2);
+console.log(2 / 2);
+console.log(2 % 2);
+
+let str1 = "Ehtesham";
+let str2 = " Akbar";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+
+console.log("Hi" + " Hello");
+
+let operator = "1" + 2;
+let operatorTwo = 1 + "2";
+let finalOperator = (operator + operatorTwo);
+
+console.log(finalOperator);
+
+
+console.log("1" + 2); // String + Number = Join
+console.log(1 + "2"); // Number + String = Join
+console.log("1" + "2"); // String + String = Join
+console.log("1" + 2 + 3); // Output: "123" This is JS Rule
+console.log(1 + 2 + "3"); // Output: "33" This is JS Rule
+console.log(1 == 2); // Output: "false" This is not equal value
+console.log(4 == 4); // Output: "True" This is equal value
+console.log("8" === 8); // false (string vs number) 3 === Type bhi Check krte hen aur == Sirf Value;
+console.log("10" === "10"); // true (string vs string)
+console.log(10 !== 10); // // 10 !== 10 → false (dono same hain) ye Ulta hisab he yah sahi value woh galat batata he aur galat value ko sahi
+console.log(2 > 9); // 2 > 9 → false (2 is not greater than 9)
+console.log(5 < 2); // 5 < 2 → false (5 is not less than 2)
+console.log(9 >= 2); // bada ya barabar hona chahiye
+console.log(2 <= 5); // Chhota ya barabar hona chahiye
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+let minusNum = 99;
+minusNum--;
+console.log(minusNum);
+
