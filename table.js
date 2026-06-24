bdddc983ef6cf09408e0f4530ca5203a92231518
Ehtesham Akbar <youremail@example.com>
2026-06-24 08:54:59 +0100

    Table for loop

---
+++ table.js
@@ -0,0 +1,14 @@
+
+
+
+for (let rowTable = 2; rowTable <= 2; rowTable++) {
+    for (let columnTable = 1; columnTable <= 10; columnTable++) {
+        console.log(`${rowTable} x ${columnTable} = ${rowTable * columnTable}`);
+    }
+}
+
+
+
+
+
+
